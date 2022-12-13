a0059e1cb8a7dbfa430cc1dabaf99d1738972ff9
vandamedeveloper <99797005+vandamedeveloper@users.noreply.github.com>
2022-12-13 13:49:03 +0100

    Feat/authentication (#4)
    * feat(authentication): try something else
    
    * feat(authentication): added authentication on login screen
    
    * feat(authentication): changed to removeItem
    
    Co-authored-by: vbeanf <vandiamirovic@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { firebaseConfig } from 'config/firebaseConfig';
+import { AuthenticationService } from './core/authentication/feature/src/lib/services/authentication.service';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+  ],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/authentication/feature/src/lib/components/login/login.component.ts
+++ src/app/core/authentication/feature/src/lib/components/login/login.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
 @Component({
   selector: 'studioos-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
 
+  constructor(
+    private authService: AuthenticationService,
+    private _fBuilder: FormBuilder
+  ) {}
+
+  signIn() {
+    try {
+      this.authService.signIn(
+        this.loginForm.controls['email'].value,
+        this.loginForm.controls['password'].value
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  _createForm() {
+    return this._fBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this._createForm();
+  }
 }
